1848e84c9663dc67adefa586f157ba37676d9688
EgorChernov3140 <egorchernov3140@gmail.com>
2023-06-25 01:37:38 +0300

    update script

--- web/fer_project/static/js/script.js
+++ web/fer_project/static/js/script.js
@@ -7,8 +7,10 @@ var currentTime = null
 const fps = 1
 // Выбирает разрешение камеры
 var constraints = { video: { width: 850, height: 500, facingMode: 'user' } }
+var result = null
+var time = null
 
-if (window.location.pathname === '/test_webcam/') {
+if (window.location.pathname === '/record/') {
     console.log("Webcam is working")
     navigator.mediaDevices.getUserMedia(constraints)
     .then(function(mediaStream) {
@@ -24,12 +26,14 @@ if (window.location.pathname === '/test_webcam/') {
         // check for errors at the end.
         console.log(err.name + ': ' + err.message)
     })
+} else if (window.location.pathname === '/result/') {
+    getResult()
 }
 
 
 function listenPage() {
     if (isStream) {
-        if (window.location.pathname !== '/test_webcam/') {
+        if (window.location.pathname !== '/record/') {
             isStream = false
             clearInterval(listenerInterval)
             console.log("Webcam was stopped")
@@ -42,11 +46,14 @@ function recordStream() {
     console.log('Start record')
     var start_button = document.querySelector('#start_button')
     var stop_button = document.querySelector('#stop_button')
+    var result_button = document.querySelector('#result_button')
 
     isRecord = true
     start_button.classList.add('d-none')
     stop_button.classList.remove('d-none')
-    console.log(isRecord)
+    result_button.classList.add('d-none')
+
+    result = {angry: 0, disgust: 0, fear: 0, happy: 0, neutral: 0, sad: 0, surprise: 0}
 
     cameraInterval = setInterval(function() { sendFrame() }, 1000/fps)
     currentTime = new Date().getTime()
@@ -58,27 +65,30 @@ function stopRecordStream() {
     console.log('Stop record')
     var start_button = document.querySelector('#start_button')
     var stop_button = document.querySelector('#stop_button')
+    var result_button = document.querySelector('#result_button')
 
     isRecord = false
     stop_button.classList.add('d-none')
     start_button.classList.remove('d-none')
-    console.log(isRecord)
+    result_button.classList.remove('d-none')
 
     clearInterval(cameraInterval)
     clearInterval(timerInterval)
     document.getElementById("timer").innerHTML = "0h 0m 0s"
+
+    localStorage.setItem("result", JSON.stringify(result))
+    localStorage.setItem("time", time)
 }
 
 
 async function postPredict(data) {
     try {
-        const response = await fetch("/test_hello/", {
+        const response = await fetch("/predict/", {
             method: "POST",
             body: data,
         })
 
         const result = await response.text()
-        console.log("Success")
         return result
     } catch (error) {
         console.error("Error:", error)
@@ -100,7 +110,10 @@ async function sendFrame() {
 
     dataURL = dataURL.replace('data:image/png;base64,', '')
 
-    let response = postPredict(dataURL)
+    let response = await postPredict(dataURL)
+    if (response !== 'None') {
+        result[response] = result[response] + 1
+    }
 }
 
 // Update the count down every 1 second
@@ -117,5 +130,44 @@ function startTimer() {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
     // Display the result in the element with id="demo"
-    document.getElementById("timer").innerHTML = hours + "h " + minutes + "m " + seconds + "s"
+    time = hours + "h " + minutes + "m " + seconds + "s"
+    document.getElementById("timer").innerHTML = time
+}
+
+function getResult() {
+    window.onload = function() {
+        let rec_time = localStorage.getItem("time")
+        let rec_res = localStorage.getItem("result")
+        rec_res = JSON.parse(rec_res)
+        if (rec_time) {
+            document.getElementById("time").innerHTML = rec_time
+
+            google.load("visualization", "1", {packages:["corechart"]})
+            google.setOnLoadCallback(drawChart)
+            function drawChart() {
+                var data = google.visualization.arrayToDataTable([
+                    ['Emotion', 'Graph of mood changes'],
+                    ['Angry', rec_res['angry']],
+                    ['Disgust', rec_res['disgust']],
+                    ['Fear', rec_res['fear']],
+                    ['Happy', rec_res['happy']],
+                    ['Neutral', rec_res['neural']],
+                    ['Sad', rec_res['sad']],
+                    ['Surprise', rec_res['surprise']],
+                ])
+
+                var options = {
+                    pieHole: 0.5,
+                    pieSliceTextStyle: {
+                        color: 'black',
+                    },
+                    backgroundColor: 'transparent',
+                    colors: ['#a81e11','#507350','#2c1970','#c9bd38','#89c5d6','#272e30','#b35415',]
+                }
+
+                var chart = new google.visualization.PieChart(document.getElementById('donut_single'))
+                chart.draw(data, options)
+            }
+        }
+    }
 }
