df051462bed3ee87b0ee48cc74bb31b81e350317
EgorChernov3140 <egorchernov3140@gmail.com>
2023-06-24 19:34:22 +0300

    connect front with back and send stream

--- web/fer_project/main_app/views.py
+++ web/fer_project/main_app/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.views.decorators import gzip
-from django.http import StreamingHttpResponse
+from django.http import StreamingHttpResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-from .utils import VideoCamera, gen
+import io
+import base64
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+import cv2
 
 
-def main_view(request):
-    return render(request, 'main_app/main.html')
+faceCascade = cv2.CascadeClassifier('static/model/haarcascade_frontalface_default.xml')
 
 
-def test(request):
+def welcome(request):
     return render(request, 'main_app/welcome.html')
 
 
@@ -17,12 +22,58 @@ def webcam(request):
     return render(request, 'main_app/webcam.html')
 
 
-@gzip.gzip_page
-def video_stream(request):
-    try:
-        cam = VideoCamera()
-        return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
-    except:  # This is bad! replace it with proper handling
-        pass
+# @gzip.gzip_page
+# def video_stream(request):
+#     try:
+#         cam = VideoCamera()
+#         return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
+#     except:  # This is bad! replace it with proper handling
+#         print('Smthg wrong...')
+#         pass
+#
+#     return render(request, 'main_app/welcome.html')
 
-    return render(request, 'main_app/main.html')
+
+def test_webcam(request):
+    return render(request, 'main_app/test_webcam.html')
+
+
+@csrf_exempt
+def test_hello(request):
+    response = None
+    if request.method == 'POST':
+        # Decode and convert into np.array
+        img = io.BytesIO(base64.b64decode(request.body))
+        img = Image.open(img)
+        img = np.array(img)
+
+        # Convert to grayscale
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Crop image
+        face = faceCascade.detectMultiScale(img,
+                                            scaleFactor=1.1,
+                                            minNeighbors=5,
+                                            minSize=(20, 20))
+        x, y, w, h = face[0]
+        img = img[y:(y + h), x:(x + w)]
+
+        # model = tf.keras.models.load_model("model/model.h5")
+        # model.predict()
+
+        # cv2.imwrite('contour.png', img)
+        print(img.shape)
+
+        # ## converting RGB to BGR, as opencv standards
+        # frame = cv2.cvtColor(np.array(img), cv2.G)
+        #
+        # # Process the image frame
+        # imgencode = cv2.imencode('.jpg', frame)[1]
+        #
+        # # base64 encode
+        # stringData = base64.b64encode(imgencode).decode('utf-8')
+        # b64_src = 'data:image/jpg;base64,'
+        # stringData = b64_src + stringData
+
+        response = str([1, 2])
+    return HttpResponse(response)
--- web/fer_project/static/js/script.js
+++ web/fer_project/static/js/script.js
@@ -1,17 +1,121 @@
+var isStream = false
+var isRecord = false
+var cameraInterval = null
+var timerInterval = null
+var listenerInterval = null
+var currentTime = null
+const fps = 1
 // Выбирает разрешение камеры
 var constraints = { video: { width: 850, height: 500, facingMode: 'user' } }
 
-navigator.mediaDevices.getUserMedia(constraints)
+if (window.location.pathname === '/test_webcam/') {
+    console.log("Webcam is working")
+    navigator.mediaDevices.getUserMedia(constraints)
     .then(function(mediaStream) {
         var video = document.querySelector('video')
         video.srcObject = mediaStream
         video.onloadedmetadata = function(e) {
             video.play()
         }
+        isStream = true
+        listenerInterval = setInterval(function() { listenPage() }, 1000)
     })
     .catch(function(err) {
         // check for errors at the end.
         console.log(err.name + ': ' + err.message)
     })
+}
 
-console.log(document.querySelector('button.start'))
+
+function listenPage() {
+    if (isStream) {
+        if (window.location.pathname !== '/test_webcam/') {
+            isStream = false
+            clearInterval(listenerInterval)
+            console.log("Webcam was stopped")
+        }
+    }
+}
+
+
+function recordStream() {
+    console.log('Start record')
+    var start_button = document.querySelector('#start_button')
+    var stop_button = document.querySelector('#stop_button')
+
+    isRecord = true
+    start_button.classList.add('d-none')
+    stop_button.classList.remove('d-none')
+    console.log(isRecord)
+
+    cameraInterval = setInterval(function() { sendFrame() }, 1000/fps)
+    currentTime = new Date().getTime()
+    timerInterval = setInterval(function() { startTimer() }, 1000)
+}
+
+
+function stopRecordStream() {
+    console.log('Stop record')
+    var start_button = document.querySelector('#start_button')
+    var stop_button = document.querySelector('#stop_button')
+
+    isRecord = false
+    stop_button.classList.add('d-none')
+    start_button.classList.remove('d-none')
+    console.log(isRecord)
+
+    clearInterval(cameraInterval)
+    clearInterval(timerInterval)
+    document.getElementById("timer").innerHTML = "0h 0m 0s"
+}
+
+
+async function postPredict(data) {
+    try {
+        const response = await fetch("/test_hello/", {
+            method: "POST",
+            body: data,
+        })
+
+        const result = await response.text()
+        console.log("Success")
+        return result
+    } catch (error) {
+        console.error("Error:", error)
+        return null
+    }
+}
+
+
+async function sendFrame() {
+    var canvas = document.querySelector('canvas')
+    var video = document.querySelector('video')
+    var screenshotImage = document.querySelector('img')
+
+    canvas.width = video.videoWidth
+    canvas.height = video.videoHeight
+    canvas.getContext('2d').drawImage(video, 0, 0)
+    var dataURL = canvas.toDataURL()
+    screenshotImage.src = dataURL
+
+    dataURL = dataURL.replace('data:image/png;base64,', '')
+
+    let response = postPredict(dataURL)
+}
+
+// Update the count down every 1 second
+function startTimer() {
+    // Get today's date and time
+    var now = new Date().getTime()
+
+    // Find the distance between now and the count down date
+    var distance = now - currentTime
+
+    // Time calculations for days, hours, minutes and seconds
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000)
+
+    // Display the result in the element with id="demo"
+    document.getElementById("timer").innerHTML = hours + "h " + minutes + "m " + seconds + "s"
+}
