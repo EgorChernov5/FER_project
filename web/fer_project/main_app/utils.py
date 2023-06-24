aec583b3d6e9f9219a78e2c2d60159bface43688
EgorChernov3140 <egorchernov3140@gmail.com>
2023-06-25 01:41:16 +0300

    refactor

--- web/fer_project/main_app/utils.py
+++ web/fer_project/main_app/utils.py
@@ -1,28 +1,43 @@
+import tensorflow as tf
 import cv2
-import threading
+import base64
+from PIL import Image
+import numpy as np
+import io
 
 
-class VideoCamera(object):
-    def __init__(self):
-        self.video = cv2.VideoCapture(0)
-        (self.grabbed, self.frame) = self.video.read()
-        threading.Thread(target=self.update, args=()).start()
-
-    def __del__(self):
-        self.video.release()
-
-    def get_frame(self):
-        image = self.frame
-        _, jpeg = cv2.imencode('.jpg', image)
-        return jpeg.tobytes()
-
-    def update(self):
-        while True:
-            (self.grabbed, self.frame) = self.video.read()
+faceCascade = cv2.CascadeClassifier('static/model/haarcascade_frontalface_default.xml')
+CLASS_NAMES = np.array(['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise'])
 
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield(b'--frame\r\n'
-              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+def decode_image(b_image):
+    img = io.BytesIO(base64.b64decode(b_image))
+    img = Image.open(img).convert('RGB')
+    return np.array(img)
+
+
+def preprocess_image(image):
+    # Convert to grayscale
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Crop image
+    bbox = faceCascade.detectMultiScale(gray_image,
+                                        scaleFactor=1.1,
+                                        minNeighbors=5,
+                                        minSize=(20, 20))
+    if len(bbox) > 0:
+        x, y, w, h = bbox[0]
+        image = tf.image.rgb_to_grayscale(image)
+        image = image[y:(y + h), x:(x + w), :]
+        image = tf.image.resize(image, [48, 48])
+        # Add batch size
+        image = np.expand_dims(image, axis=0)
+        return image
+
+    return None
+
+
+def get_predict(image):
+    model = tf.keras.models.load_model("static/model/model.h5")
+    y_pred = model.predict(tf.convert_to_tensor(image))
+    predict = CLASS_NAMES[np.argmax(y_pred[0])]
+    return predict
--- web/fer_project/main_app/views.py
+++ web/fer_project/main_app/views.py
@@ -1,79 +1,29 @@
 from django.shortcuts import render
-from django.views.decorators import gzip
-from django.http import StreamingHttpResponse, HttpResponse
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
-import io
-import base64
-import tensorflow as tf
-from PIL import Image
-import numpy as np
-import cv2
-
-
-faceCascade = cv2.CascadeClassifier('static/model/haarcascade_frontalface_default.xml')
+from .utils import decode_image, preprocess_image, get_predict
 
 
 def welcome(request):
     return render(request, 'main_app/welcome.html')
 
 
-def webcam(request):
-    return render(request, 'main_app/webcam.html')
+def result(request):
+    return render(request, 'main_app/result.html')
 
 
-# @gzip.gzip_page
-# def video_stream(request):
-#     try:
-#         cam = VideoCamera()
-#         return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
-#     except:  # This is bad! replace it with proper handling
-#         print('Smthg wrong...')
-#         pass
-#
-#     return render(request, 'main_app/welcome.html')
-
-
-def test_webcam(request):
-    return render(request, 'main_app/test_webcam.html')
+def record(request):
+    return render(request, 'main_app/record.html')
 
 
 @csrf_exempt
-def test_hello(request):
+def predict(request):
     response = None
     if request.method == 'POST':
-        # Decode and convert into np.array
-        img = io.BytesIO(base64.b64decode(request.body))
-        img = Image.open(img)
-        img = np.array(img)
+        img = decode_image(request.body)
+        img = preprocess_image(img)
+        if img is not None:
+            response = get_predict(img)
 
-        # Convert to grayscale
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        # Crop image
-        face = faceCascade.detectMultiScale(img,
-                                            scaleFactor=1.1,
-                                            minNeighbors=5,
-                                            minSize=(20, 20))
-        x, y, w, h = face[0]
-        img = img[y:(y + h), x:(x + w)]
-
-        # model = tf.keras.models.load_model("model/model.h5")
-        # model.predict()
-
-        # cv2.imwrite('contour.png', img)
-        print(img.shape)
-
-        # ## converting RGB to BGR, as opencv standards
-        # frame = cv2.cvtColor(np.array(img), cv2.G)
-        #
-        # # Process the image frame
-        # imgencode = cv2.imencode('.jpg', frame)[1]
-        #
-        # # base64 encode
-        # stringData = base64.b64encode(imgencode).decode('utf-8')
-        # b64_src = 'data:image/jpg;base64,'
-        # stringData = b64_src + stringData
-
-        response = str([1, 2])
     return HttpResponse(response)
