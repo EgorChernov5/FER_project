ed05d37baa51521bbbf2f152cc6e5afd363c8d22
EgorChernov3140 <egorchernov3140@gmail.com>
2023-06-25 12:47:52 +0300

    add description

--- web/fer_project/main_app/utils.py
+++ web/fer_project/main_app/utils.py
@@ -11,24 +11,47 @@ CLASS_NAMES = np.array(['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', '
 
 
 def decode_image(b_image):
+    """
+    Decode bytes to image.
+
+    Args:
+        b_image: bytes (base64).
+
+    Return:
+         Numpy array image.
+    """
     img = io.BytesIO(base64.b64decode(b_image))
     img = Image.open(img).convert('RGB')
     return np.array(img)
 
 
 def preprocess_image(image):
-    # Convert to grayscale
+    """
+    Process image for the model format.
+
+    Args:
+        image: image (np.array).
+
+    Return:
+        Cropped scaled and resized image or None.
+    """
+    # Convert to grayscale for detection
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # Crop image
+    # Get the bounding box
     bbox = faceCascade.detectMultiScale(gray_image,
                                         scaleFactor=1.1,
                                         minNeighbors=5,
                                         minSize=(20, 20))
-    if len(bbox) > 0:
+    if len(bbox):
         x, y, w, h = bbox[0]
+        # Convert to grayscale for classification
         image = tf.image.rgb_to_grayscale(image)
+        # Crop the image
         image = image[y:(y + h), x:(x + w), :]
+        # Resize the image
         image = tf.image.resize(image, [48, 48])
+        # Scale the image
+        image = tf.cast(image, dtype=tf.float32) / tf.constant(256, dtype=tf.float32)
         # Add batch size
         image = np.expand_dims(image, axis=0)
         return image
@@ -37,6 +60,15 @@ def preprocess_image(image):
 
 
 def get_predict(image):
+    """
+    Get predict.
+
+    Args:
+        image: image [b, h, w, c].
+
+    Return:
+         Predict (str).
+    """
     model = tf.keras.models.load_model("static/model/model.h5")
     y_pred = model.predict(tf.convert_to_tensor(image))
     predict = CLASS_NAMES[np.argmax(y_pred[0])]
--- web/fer_project/static/js/script.js
+++ web/fer_project/static/js/script.js
@@ -1,36 +1,40 @@
 var isStream = false
 var isRecord = false
+
 var cameraInterval = null
 var timerInterval = null
 var listenerInterval = null
-var currentTime = null
-const fps = 1
-// Выбирает разрешение камеры
-var constraints = { video: { width: 850, height: 500, facingMode: 'user' } }
-var result = null
-var time = null
 
+var result = null
+var startTime = null
+var time = null
+// Set the params for the webcam
+var constraints = { video: { width: 850, height: 500, facingMode: 'user' } }
+// Get a video stream if the page is intended for recording
 if (window.location.pathname === '/record/') {
     console.log("Webcam is working")
     navigator.mediaDevices.getUserMedia(constraints)
     .then(function(mediaStream) {
+        // Set a video stream in the video tag
         var video = document.querySelector('video')
         video.srcObject = mediaStream
         video.onloadedmetadata = function(e) {
             video.play()
         }
+        // Track the page
         isStream = true
         listenerInterval = setInterval(function() { listenPage() }, 1000)
     })
     .catch(function(err) {
-        // check for errors at the end.
+        // Check for errors at the end.
         console.log(err.name + ': ' + err.message)
     })
 } else if (window.location.pathname === '/result/') {
+    // Get the result if the page changes
     getResult()
 }
 
-
+// The stream is closed if the user goes to another page
 function listenPage() {
     if (isStream) {
         if (window.location.pathname !== '/record/') {
@@ -41,7 +45,7 @@ function listenPage() {
     }
 }
 
-
+// Start record stream
 function recordStream() {
     console.log('Start record')
     var start_button = document.querySelector('#start_button')
@@ -52,15 +56,16 @@ function recordStream() {
     start_button.classList.add('d-none')
     stop_button.classList.remove('d-none')
     result_button.classList.add('d-none')
-
-    result = {angry: 0, disgust: 0, fear: 0, happy: 0, neutral: 0, sad: 0, surprise: 0}
-
-    cameraInterval = setInterval(function() { sendFrame() }, 1000/fps)
-    currentTime = new Date().getTime()
+    // Init the result dict
+    result = {angry: 0, disgust: 0, fear: 0, happy: 0, neutral: 0, sad: 0, surprise: 0, nothing: 0}
+    // Send frames every second
+    cameraInterval = setInterval(function() { sendFrame() }, 1000)
+    // Start timer
+    startTime = new Date().getTime()
     timerInterval = setInterval(function() { startTimer() }, 1000)
 }
 
-
+// Stop record
 function stopRecordStream() {
     console.log('Stop record')
     var start_button = document.querySelector('#start_button')
@@ -71,23 +76,23 @@ function stopRecordStream() {
     stop_button.classList.add('d-none')
     start_button.classList.remove('d-none')
     result_button.classList.remove('d-none')
-
+    // Clear intervals
     clearInterval(cameraInterval)
     clearInterval(timerInterval)
     document.getElementById("timer").innerHTML = "0h 0m 0s"
-
+    // Add the result and the recording time to the localStorage
     localStorage.setItem("result", JSON.stringify(result))
     localStorage.setItem("time", time)
 }
 
-
+// Send the POST request
 async function postPredict(data) {
     try {
         const response = await fetch("/predict/", {
             method: "POST",
             body: data,
         })
-
+        // Convert the response to a str and return
         const result = await response.text()
         return result
     } catch (error) {
@@ -96,52 +101,54 @@ async function postPredict(data) {
     }
 }
 
-
+// Send frames to the server
 async function sendFrame() {
     var canvas = document.querySelector('canvas')
     var video = document.querySelector('video')
-    var screenshotImage = document.querySelector('img')
-
+    // Save the frame on the canvas
     canvas.width = video.videoWidth
     canvas.height = video.videoHeight
     canvas.getContext('2d').drawImage(video, 0, 0)
     var dataURL = canvas.toDataURL()
-    screenshotImage.src = dataURL
-
+    // Get bytes
     dataURL = dataURL.replace('data:image/png;base64,', '')
-
+    // Send the POST request
     let response = await postPredict(dataURL)
-    if (response !== 'None') {
+    // Process the response, update the result
+    if (response === 'None') {
+        result['nothing'] = result['nothing'] + 1
+    } else {
         result[response] = result[response] + 1
     }
 }
 
-// Update the count down every 1 second
+// Update the countdown every second
 function startTimer() {
     // Get today's date and time
-    var now = new Date().getTime()
-
-    // Find the distance between now and the count down date
-    var distance = now - currentTime
-
-    // Time calculations for days, hours, minutes and seconds
+    var currentTime = new Date().getTime()
+    // Find the distance between now and the countdown date
+    var distance = currentTime - startTime
+    // Time calculations for hours, minutes and seconds
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
     var seconds = Math.floor((distance % (1000 * 60)) / 1000)
-
-    // Display the result in the element with id="demo"
+    // Display the result in the element with id="timer"
     time = hours + "h " + minutes + "m " + seconds + "s"
     document.getElementById("timer").innerHTML = time
 }
 
+// Get the result and display it
 function getResult() {
+    // Monitor the loading of the window
     window.onload = function() {
+        // Get data from localStorage
         let rec_time = localStorage.getItem("time")
         let rec_res = localStorage.getItem("result")
         rec_res = JSON.parse(rec_res)
         if (rec_time) {
+            // Display the time
             document.getElementById("time").innerHTML = rec_time
-
+            // Draw the chart
             google.load("visualization", "1", {packages:["corechart"]})
             google.setOnLoadCallback(drawChart)
             function drawChart() {
@@ -154,17 +161,13 @@ function getResult() {
                     ['Neutral', rec_res['neural']],
                     ['Sad', rec_res['sad']],
                     ['Surprise', rec_res['surprise']],
+                    ['Not recognized', rec_res['nothing']],
                 ])
-
                 var options = {
                     pieHole: 0.5,
-                    pieSliceTextStyle: {
-                        color: 'black',
-                    },
                     backgroundColor: 'transparent',
-                    colors: ['#a81e11','#507350','#2c1970','#c9bd38','#89c5d6','#272e30','#b35415',]
+                    colors: ['#a81e11','#316131','#3d1d59','#e07707','#848c8c','#2d3573','#874c80', '#1e1f1e',]
                 }
-
                 var chart = new google.visualization.PieChart(document.getElementById('donut_single'))
                 chart.draw(data, options)
             }
