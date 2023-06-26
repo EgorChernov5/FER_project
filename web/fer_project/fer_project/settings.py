9b7429d010bd5d55ddb0300bfee320984d6050b6
EgorChernov3140 <egorchernov3140@gmail.com>
2023-06-26 21:48:05 +0300

    update

--- web/fer_project/fer_project/settings.py
+++ web/fer_project/fer_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5l2+*@8dm4a-zf(&v9)x=vb)gryi(9&00pg(v9r5i97ognr5u)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['d6cc-5-18-234-152.ngrok-free.app', '127.0.0.1']
+ALLOWED_HOSTS = ['*', '0.0.0.0', '127.0.0.1']
 
 
 # Application definition
--- web/fer_project/main_app/urls.py
+++ web/fer_project/main_app/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 
 urlpatterns = [
+    path('', welcome),
     path('fer_project/', welcome, name='welcome'),
     path('result/', result, name='result'),
     path('record/', record, name='record'),
