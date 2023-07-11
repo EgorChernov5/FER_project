243159d4ebc5d194c2430512e6e7da6def900093
EgorChernov3140 <egorchernov3140@gmail.com>
2023-07-11 22:58:30 +0300

    Add rescale function.

--- ml/util/util.py
+++ ml/util/util.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import json
+import tensorflow as tf
 
 
 REPO_DIR = Path(__file__).parent.parent
@@ -15,3 +16,10 @@ def open_json(path):
 def save_as_json(path, data):
     with open(path, "w") as json_file:
         json.dump(data, json_file)
+
+
+def rescale(image, a=0., b=1.):
+    max_pixel = tf.math.reduce_max(image)
+    min_pixel = tf.math.reduce_min(image)
+    rescale_image = (image - min_pixel) / (max_pixel - min_pixel)
+    return rescale_image * (b - a) + a
